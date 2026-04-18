c5db4fc77dfcdf478539123245d5d342eceb83d3
tbotalla <tbotalla@fi.uba.ar>
2026-04-18 00:06:25 -0300

    Tp Final - Minor refactor rename api_buzzer functions

--- trabajo-final/Drivers/BUZZER/Inc/api_buzzer.h
+++ trabajo-final/Drivers/BUZZER/Inc/api_buzzer.h
@@ -2,12 +2,12 @@
 #define API_BUZZER_H
 
 /* Initialize buzzer output to OFF state. */
-void api_buzzer_init(void);
+void buzzer_init(void);
 
 /* Buzzer output to active level (sound ON). */
-void api_buzzer_on(void);
+void buzzer_on(void);
 
 /* Buzzer output to inactive level (sound OFF). */
-void api_buzzer_off(void);
+void buzzer_off(void);
 
 #endif /* API_BUZZER_H */
